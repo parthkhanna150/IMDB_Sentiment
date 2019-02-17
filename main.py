eb1238f297b2a7e059d218e0af4333a670a39c3c
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2019-02-16 19:19:33 -0500

    aa

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
+# copy contents of all files in both folders into a list
 train_data = []
 test_data = []
 
@@ -25,53 +27,44 @@ train_pos = glob.glob(os.path.join(os.getcwd(), "Dataset/train/pos", "*.txt"))
 for f_path in train_pos:
     with open(f_path) as f:
         train_data.append(f.read())
+# print(train_data[0])
+# print(preprocess(train_data[0]))
 
 # test data
-test_files = glob.glob(os.path.join(os.getcwd(), "Dataset/test", "*.txt"))
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+test_files = (glob.glob(os.path.join(os.getcwd(), "Dataset/test", "*.txt")))
 for f_path in test_files:
     with open(f_path) as f:
         test_data.append(f.read())
 
 # targets: first 12500 are pos, next 12500 are neg
-targets = [0 if i<12500 else 1 for i in range(25000)]
+targets = [1 if i<12500 else 0 for i in range(25000)]
 
-train_data_clean = preprocess(train_data, advanced=False)
-test_data_clean = preprocess(test_data, advanced=False)
+train_data_clean = preprocess(train_data)
+test_data_clean = preprocess(test_data)
 
 # splitting the data
-X_train, X_validation, y_train, y_validation = train_test_split(train_data, targets, train_size=0.8, test_size=0.2, random_state=551)
+X_train, X_validation, y_train, y_validation = train_test_split(train_data, targets, train_size=0.8, test_size=0.2, random_state=1)
 
-# Bag of Words vectorization
-cv = CountVectorizer().fit(X_train)
-X_train_counts = cv.transform(X_train)
-X_validation_counts = cv.transform(X_validation)
+pclf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('norm', Normalizer()),
+    # ('clf', LogisticRegression()),
+    ('clf', LinearSVC()),
+])
 
-# tfidf
-tfidf_transformer = TfidfTransformer().fit(X_train_counts)
-X_train_tfidf = tfidf_transformer.transform(X_train_counts)
-X_validation_tfidf = tfidf_transformer.transform(X_validation_counts)
-
-# normalization
-normalizer_tranformer = Normalizer().fit(X=X_train_tfidf)
-X_train_normalized = normalizer_tranformer.transform(X_train_tfidf)
-X_validation_normalized = normalizer_tranformer.transform(X_validation_tfidf)
-
-def display_results(y_val, y_pred, heading):
+def display_results(y_val, y_pred):
     print(metrics.classification_report(y_val, y_pred))
     print("Accuracy % = ", metrics.accuracy_score(y_val, y_pred))
 
-# clf_NB = MultinomialNB().fit(X_train_normalized, y_train)
-# y_pred = clf_NB.predict(X_validation_normalized)
-# display_results(y_validation, y_pred,"")
+pclf.fit(X_train, y_train)
+y_pred = pclf.predict(test_data_clean)
+print(y_pred)
 
-clf_LR = LogisticRegression().fit(X_train_normalized, y_train)
-y_pred = clf_LR.predict(X_validation_normalized)
-display_results(y_validation, y_pred,"")
-
-# clf_DT = DecisionTreeClassifier().fit(X_train_normalized, y_train)
-# y_pred = clf_DT.predict(X_validation_normalized)
-# display_results(y_validation, y_pred,"")
-
-clf_SVM = LinearSVC().fit(X_train_normalized, y_train)
-y_pred = clf_SVM.predict(X_validation_normalized)
-display_results(y_validation, y_pred,"")
+# display_results(y_validation, y_pred)
--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,12 @@
-from bs4 import BeautifulSoup
 import re, string, unicodedata
-from nltk import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import LancasterStemmer, WordNetLemmatizer
+# import nltk
+# nltk.download('stopwords')
+# import contractions
+# import inflect
+from bs4 import BeautifulSoup
+# from nltk import word_tokenize
+# from nltk.corpus import stopwords
+# from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 def remove_html(text):
     soup = BeautifulSoup(text, "html.parser")
@@ -16,19 +20,81 @@ def remove_links_characters(text):
 def regular_preprocess(text):
     text = remove_html(text)
     text = remove_links_characters(text)
+    # text = stem_words(text.split())
+    # text = replace_contractions(text)
     return text
 
-def advanced_preprocess(text):
-    return text
+# def advanced_preprocess(text):
+# #     words = nltk.word_tokenize(text)
+#     words = replace_numbers(text)
+#     words = remove_non_ascii(words)
+#     words = to_lowercase(words)
+#     words = remove_stopwords(words)
+#     words = stem_words(words)
+#     return words
 
-def preprocess(data, advanced=False):
-    refined_data=[]
+# def replace_contractions(text):
+#     """Replace contractions in string of text"""
+#     return contractions.fix(text)
+#
+# def stem_words(words):
+#     """Stem words in list of tokenized words"""
+#     stemmer = PorterStemmer()
+#     stems = []
+#     for word in words:
+#         stem = stemmer.stem(word)
+#         stems.append(stem)
+#     return stems
+#
+# def remove_stopwords(words):
+#     """Remove stop words from list of tokenized words"""
+#     new_words = []
+#     for word in words:
+#         if word not in stopwords.words('english'):
+#             new_words.append(word)
+#     return new_words
+#
+# def replace_numbers(words):
+#     """Replace all interger occurrences in list of tokenized words with textual representation"""
+#     p = inflect.engine()
+#     new_words = []
+#     for word in words:
+#         if word.isdigit():
+#             new_word = p.number_to_words(word)
+#             new_words.append(new_word)
+#         else:
+#             new_words.append(word)
+#     return new_words
+#
+# def lemmatize_verbs(words):
+#     """Lemmatize verbs in list of tokenized words"""
+#     lemmatizer = WordNetLemmatizer()
+#     lemmas = []
+#     for word in words:
+#         lemma = lemmatizer.lemmatize(word, pos='v')
+#         lemmas.append(lemma)
+#     return lemmas
+#
+# def to_lowercase(words):
+#     """Convert all characters to lowercase from list of tokenized words"""
+#     new_words = []
+#     for word in words:
+#         new_word = word.lower()
+#         new_words.append(new_word)
+#     return new_words
+#
+# def remove_non_ascii(words):
+#     """Remove non-ASCII characters from list of tokenized words"""
+#     new_words = []
+#     for word in words:
+#         new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+#         new_words.append(new_word)
+#     return new_words
 
-    if advanced:
-        for data_point in data:
-            refined_data.append(advanced_preprocess(data_point))
-    else:
-        for data_point in data:
-            refined_data.append(regular_preprocess(data_point))
+def preprocess(data):
+    refined_data = []
+    for dp in data:
+        refined_data.append(regular_preprocess(dp))
 
+    # refined_data = advanced_preprocess(refined_data)
     return refined_data
